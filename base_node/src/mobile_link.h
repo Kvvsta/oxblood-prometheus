444f1366b27708c3d18e7488d6e53fcf8127bc27
Josh McAllan <j.mcallan@student.uq.edu.au>
2026-05-20 14:40:26 +1000

    Fixed the function prototype based on reflected changes from source file.

--- base_node/src/mobile_link.h
+++ base_node/src/mobile_link.h
@@ -2,11 +2,11 @@
 #define MOBILE_LINK_H
 
 #include <stdbool.h>
-#include <zephyr/kernel.h>
+#include <stddef.h>
 #include "imu_packet.h"
 
-void mobile_link_init(struct k_msgq *target_q);
 void mobile_link_start(void);
-bool mobile_link_process_imu_packet(const char *node_name, const struct imu_packet *pkt);
+bool mobile_link_process_imu_packet(const char *node_name, const struct imu_packet *pkt,
+        size_t len);
 
 #endif /* MOBILE_LINK_H */
