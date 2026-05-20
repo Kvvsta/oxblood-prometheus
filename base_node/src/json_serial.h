94ca28ef8b818bff4d0bc78be9bc4791420f289b
Josh McAllan <j.mcallan@student.uq.edu.au>
2026-05-20 14:27:45 +1000

    Updates function descriptor to reflect parameter changes in the src file.

--- base_node/src/json_serial.h
+++ base_node/src/json_serial.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 void json_serial_init(void);
-void json_emit_imu(int player, float gyro_y, float gyro_z);
+void json_emit_imu(const char* node_name, float gyro_y, float gyro_z);
 void json_emit_status(const char *status, const char *detail);
 void json_emit_audio_debug(const char *event, const char *action);
 
