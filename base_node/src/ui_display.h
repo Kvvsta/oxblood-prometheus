350bc89879fb3c3968d6b53ef09add78608e8c3e
Josh McAllan <j.mcallan@student.uq.edu.au>
2026-05-26 15:46:59 +1000

    Updates function descriptor for new function in ui display source file.

--- base_node/src/ui_display.h
+++ base_node/src/ui_display.h
@@ -5,6 +5,7 @@ void ui_display_init(void);
 void ui_display_update_imu(int player, float gy, float gz);
 void ui_display_update_audio(const char *event);
 void ui_display_update_score(int p1_score, int p2_score, int high_score);
+void ui_display_update_gesture(const char *gesture);
 void ui_display_tick(void);
 
 #endif
